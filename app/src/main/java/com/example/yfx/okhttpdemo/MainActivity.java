2ae60c8c92cc2016ea0794fba18d90dd2c9c52f7
yfx <yfx@yfxdeMacBook-Pro.local>
2017-03-21 22:28:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yfx/okhttpdemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.yfx.okhttpdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://blog.csdn.net/lmj623565791/article/details/47911083")
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                String body = response.body().string();
+                Log.i("TAG", "onResponse: "+body);
+            }
+        });
+    }
+}
